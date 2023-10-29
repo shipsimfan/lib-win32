52ecd868cbf0acbbd692d095ba158f8dcc5331da
Lance Hart <shipsimfan@hotmail.com>
2023-10-29 15:20:17 -0400

    Added bind function

--- src/winsock/socket.rs
+++ src/winsock/socket.rs
@@ -1,4 +1,8 @@
-use crate::raw::{self, closesocket};
+use crate::{
+    raw::{self, bind, closesocket},
+    Error, SockAddr,
+};
+use std::ffi::c_int;
 
 pub struct Socket {
     socket: raw::Socket,
@@ -7,6 +11,22 @@ pub struct Socket {
 impl Socket {
     pub fn new(socket: raw::Socket) -> Self {
         Socket { socket }
+    }
+
+    pub fn bind<S: SockAddr>(&self, addr: &S) -> Result<(), Error> {
+        let result = unsafe {
+            bind(
+                self.socket,
+                addr as *const _ as _,
+                std::mem::size_of::<S::Inner>() as c_int,
+            )
+        };
+
+        if result != 0 {
+            Err(Error::wsa_last_error())
+        } else {
+            Ok(())
+        }
     }
 }
 
