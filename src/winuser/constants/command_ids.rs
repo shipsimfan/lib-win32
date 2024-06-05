350ebc4601b650cb6910d358e29db41375bb55e7
Lance Hart <me@lancehart.ca>
2024-06-04 21:13:19 -0400

    Added MessageBoxExW

---
+++ src/winuser/constants/command_ids.rs
@@ -0,0 +1,34 @@
+use std::ffi::c_int;
+
+/// The "OK" button was selected
+pub const IDOK: c_int = 1;
+
+/// The "Cancel" button was selected
+pub const IDCANCEL: c_int = 2;
+
+/// The "Abort" button was selected
+pub const IDABORT: c_int = 3;
+
+/// The "Retry" button was selected
+pub const IDRETRY: c_int = 4;
+
+/// The "Ignore" button was selected
+pub const IDIGNORE: c_int = 5;
+
+/// The "Yes" button was selected
+pub const IDYES: c_int = 6;
+
+/// The "No" button was selected
+pub const IDNO: c_int = 7;
+
+/// The "Close" button was selected
+pub const IDCLOSE: c_int = 8;
+
+/// The "Help" button was selected
+pub const IDHELP: c_int = 9;
+
+/// The "Try Again" button was selected
+pub const IDTRYAGAIN: c_int = 10;
+
+/// The "Continue" button was selected
+pub const IDCONTINUE: c_int = 11;
--- src/winuser/constants/mod.rs
+++ src/winuser/constants/mod.rs
@@ -1,3 +1,7 @@
 mod class_styles;
+mod command_ids;
+mod message_box_flags;
 
 pub use class_styles::*;
+pub use command_ids::*;
+pub use message_box_flags::*;
