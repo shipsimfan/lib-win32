1e1a431a6d38d465dd23238d5af2aee130dc620c
Lance Hart <me@lancehart.ca>
2025-02-15 14:05:54 -0500

    Updated new_in function

--- src/utility/com/ptr/new_in.rs
+++ src/utility/com/ptr/new_in.rs
@@ -3,7 +3,7 @@ use std::ptr::null_mut;
 
 impl<T: ComInterface> ComPtr<T> {
     /// Creates a new [`ComPtr`] by calling `f` with an uninitialized pointer which it will fill
-    pub fn new_in<R, F: FnOnce(&mut *mut T) -> Result<R, Error>>(f: F) -> Result<ComPtr<T>, Error> {
+    pub fn new_in<R, F: FnOnce(*mut *mut T) -> Result<R, Error>>(f: F) -> Result<ComPtr<T>, Error> {
         let mut ptr = null_mut();
         f(&mut ptr).map(|_| ComPtr::new(ptr))
     }
