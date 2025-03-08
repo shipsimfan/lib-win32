1d71dcf528505ce5d327fce96d2b5e1ad00b1d1a
Lance Hart <me@lancehart.ca>
2025-03-08 13:32:03 -0500

    Added SetEvent function

--- src/synchapi/functions/mod.rs
+++ src/synchapi/functions/mod.rs
@@ -1,6 +1,7 @@
 mod cancel_waitable_timer;
 mod create_event_w;
 mod create_waitable_timer_w;
+mod set_event;
 mod set_watiable_timer;
 mod sleep_ex;
 mod wait_for_multiple_objects_ex;
@@ -10,6 +11,7 @@ pub use create_event_w::{CreateEventW, CreateEventW as CreateEvent};
 pub use create_waitable_timer_w::{
     CreateWaitableTimerW, CreateWaitableTimerW as CreateWaitableTimer,
 };
+pub use set_event::SetEvent;
 pub use set_watiable_timer::SetWaitableTimer;
 pub use sleep_ex::SleepEx;
 pub use wait_for_multiple_objects_ex::WaitForMultipleObjectsEx;
---
+++ src/synchapi/functions/set_event.rs
@@ -0,0 +1,34 @@
+use crate::{BOOL, HANDLE};
+
+// rustdoc imports
+#[allow(unused_imports)]
+use crate::{CreateEvent, GetLastError};
+
+#[link(name = "Kernel32")]
+extern "system" {
+    /// Sets the specified event object to the signaled state.
+    ///
+    /// # Parameters
+    ///  * `event` - A handle to the event object. The [`CreateEvent`] or [`OpenEvent`] function
+    ///              returns this handle. The handle must have the `EVENT_MODIFY_STATE` access
+    ///              right.
+    ///
+    /// # Return Value
+    /// If the function succeeds, the return value is nonzero.
+    ///
+    /// If the function fails, the return value is zero. To get extended error information, call
+    /// [`GetLastError`].
+    ///
+    /// # Remarks
+    /// The state of a manual-reset event object remains signaled until it is set explicitly to the
+    /// nonsignaled state by the [`ResetEvent`] function. Any number of waiting threads, or threads
+    /// that subsequently begin wait operations for the specified event object by calling one of
+    /// the wait functions, can be released while the object's state is signaled.
+    ///
+    /// In contrast, the state of an auto-reset event object remains signaled until a single
+    /// waiting thread is released, at which time the system automatically sets the state to
+    /// nonsignaled. If no threads are waiting, the event object's state remains signaled.
+    ///
+    /// Setting an event that is already set has no effect.
+    pub fn SetEvent(event: HANDLE) -> BOOL;
+}
