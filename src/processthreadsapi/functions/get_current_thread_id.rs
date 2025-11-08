f0aa2974a003e2c220e8e9dece36e95cde78cb03
Lance Hart <me@lancehart.ca>
2025-11-08 14:32:22 -0500

    Added GetCurrentThreadId

---
+++ src/processthreadsapi/functions/get_current_thread_id.rs
@@ -0,0 +1,14 @@
+use crate::DWORD;
+
+#[link(name = "Kernel32")]
+extern "system" {
+    /// Retrieves the thread identifier of the calling thread.
+    ///
+    /// # Return Value
+    /// The return value is the thread identifier of the calling thread.
+    ///
+    /// # Remarks
+    /// Until the thread terminates, the thread identifier uniquely identifies the thread
+    /// throughout the system.
+    pub fn GetCurrentThreadId() -> DWORD;
+}
